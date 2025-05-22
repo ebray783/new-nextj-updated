1fc5371b64deecba6e2df032b775012c996b41a6
ebray783 <ebray783@gmail.com>
2025-05-22 02:25:11 -0700

    Disable Cloudflare authentication across all pages

--- public/_worker.js
+++ public/_worker.js
@@ -2,5 +2,9 @@ export default {
   flags: [
     'nodejs_compat',
     'nodejs_compat_populate_process_env'
-  ]
+  ],
+  async fetch(request, env, ctx) {
+    // Disable authentication on all requests
+    return env.ASSETS.fetch(request);
+  }
 }
